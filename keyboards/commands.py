3e0843822005183899fef64371bbc3c3465a9d11
Maximon0101 <maximon2006@gmail.com>
2023-10-04 18:14:54 +0300

    add command /dialog

--- keyboards/commands.py
+++ keyboards/commands.py
@@ -4,5 +4,6 @@ from aiogram import types
 async def add_commands(bot):
     await bot.set_my_commands([
         types.bot_command.BotCommand(command="/start", description="Start the bot"),
-        types.bot_command.BotCommand(command="/info", description="Show user info and balance")
+        types.bot_command.BotCommand(command="/info", description="Show user info and balance"),
+        types.bot_command.BotCommand(command="/dialog", description="Start new dialog with bot")
     ])
