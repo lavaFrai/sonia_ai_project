906c9fbfda37204ce8a3135831ed76294931eaad
Maximon0101 <maximon2006@gmail.com>
2023-10-04 14:28:00 +0300

    add commands

--- keyboards/commands.py
+++ keyboards/commands.py
@@ -4,5 +4,5 @@ from aiogram import types
 async def add_commands(bot):
     await bot.set_my_commands([
         types.bot_command.BotCommand(command="/start", description="Start the bot"),
-        types.bot.command.BotCommand(command="/info", description="Show user info and balance")
+        types.bot_command.BotCommand(command="/info", description="Show user info and balance")
     ])
