40a31142c11a9f61256a0f73bbf40b411f671191
Maximon0101 <maximon2006@gmail.com>
2023-10-04 18:40:04 +0300

    add command /dialog

--- keyboards/commands.py
+++ keyboards/commands.py
@@ -5,5 +5,6 @@ async def add_commands(bot):
     await bot.set_my_commands([
         types.bot_command.BotCommand(command="/start", description="Start the bot"),
         types.bot_command.BotCommand(command="/info", description="Show user info and balance"),
-        types.bot_command.BotCommand(command="/dialog", description="Start new dialog with bot")
+        types.bot_command.BotCommand(command="/dialog", description="Start new dialog with bot"),
+        types.bot_command.BotCommand(command="/stop-dialog", description="Stop dialog")
     ])
