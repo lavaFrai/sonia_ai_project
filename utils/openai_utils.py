3c337ade0ec04a4fd7020e3c95688c26251462fc
lava_frai <lavafrai@yandex.ru>
2023-10-08 16:33:07 +0300

    16k tokens context size pre2.0.0

--- utils/openai_utils.py
+++ utils/openai_utils.py
@@ -3,7 +3,7 @@ import openai
 
 async def chatgpt_generate_one_message(system_prompt: str, user_prompt: str) -> str:
     response = await openai.ChatCompletion.acreate(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-16k",
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": user_prompt}
@@ -14,7 +14,7 @@ async def chatgpt_generate_one_message(system_prompt: str, user_prompt: str) -> 
 
 async def chatgpt_continue_dialog(history: dict) -> dict:
     response = await openai.ChatCompletion.acreate(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-16k",
         messages=history
     )
     return response["choices"][0]["message"]
