f512981f90215df0521eddc010a67f51c9ce85f5
lava_frai <lavafrai@yandex.ru>
2023-10-09 08:49:11 +0300

    voice support in dialogs pre2.0.0

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,3 @@
 docker stop sonia_ai_project
 docker rm sonia_ai_project
-docker run -it -d -v .:/opt/app --restart=always --name=sonia_ai_project $(docker build -v .:/opt/app -q .)
+docker run -it -d -v .:/opt/app --restart=always --name=sonia_ai_project $(docker build -q .)
